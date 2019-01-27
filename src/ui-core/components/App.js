2b7fc4fa5ec0c57703301b1b2eb89c96dd7dfdbe
codycrandall <35935539+codycrandall@users.noreply.github.com>
2019-01-26 22:53:14 -0600

    Create character (#1)
    * sass support, other misc items like ignoring css in unit tests

--- src/ui-core/components/App.js
+++ src/ui-core/components/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
 import "../sass/components/app.scss";
-
+let counter = 1;
 class App extends Component {
 	render() {
-		return <span className={"start-new-game"}>{"Start New Game"}</span>;
+		return <span className={"start-new-game"} onClick={addToCounter()}>{counter}</span>;
 	}
+
+}
+function addToCounter() {
+	counter++;
 }
 
 export default App;
