94a26959cce5d357f8d1b2f491063e30ecb8f9ce
Cody <codywaukee@gmail.com>
2019-02-05 20:36:26 -0600

    verify closed chevron is initially rendered

--- test/ui-core/components/StartNewGame.spec.js
+++ test/ui-core/components/StartNewGame.spec.js
@@ -2,24 +2,29 @@ import { expect } from 'chai';
 import React from 'react';
 import { mount } from 'enzyme';
 import {FontAwesomeIcon }from '@fortawesome/react-fontawesome';
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
+
 
 import StartNewGame from '../../../src/ui-core/components/StartNewGame';
 
 describe('<StartNewGame />', () => {
 	let startNewGame;
+
 	beforeEach(() => {
 		startNewGame = mount(<StartNewGame />);
 	});
+
 	afterEach(() => {
 		startNewGame.unmount();
 	});
+
 	it('should render the button text', () => {
 		expect(startNewGame.text()).eql('Start New Game');
 	});
 
-	it('should render a font awesome closed icon', () => {
+	it('should initially render a closed chevron', () => {
 		const fontAwesome = startNewGame.find(FontAwesomeIcon);
 		
-		expect(fontAwesome).lengthOf(1);
+		expect(fontAwesome.prop('icon')).eql(faChevronDown);
 	})
 });
