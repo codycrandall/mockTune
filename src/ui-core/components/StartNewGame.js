b9fea661e09b0d06eed330623c4ef0ef3f167f7e
Cody <codywaukee@gmail.com>
2019-02-09 16:17:27 -0600

    start new game chevron expands when clicked

--- src/ui-core/components/StartNewGame.js
+++ src/ui-core/components/StartNewGame.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import '../sass/components/StartNewGame.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons';
 
 export default function StartNewGame() {
+	const [isMenuExpanded, setMenuExpanded] = useState(false);
+
 	return (
-		<div className={'start-new-game'}>
+		<div
+			className={'start-new-game'}
+			onClick={() => setMenuExpanded(!isMenuExpanded)}
+		>
 			Start New Game
-			<FontAwesomeIcon icon={faChevronDown} size={'xs'} className={'fa-down'} />
+			<FontAwesomeIcon
+				icon={isMenuExpanded ? faChevronUp : faChevronDown}
+				size={'xs'}
+				className={`chevron`}
+			/>
 		</div>
 	);
 }
--- test/ui-core/components/StartNewGame.spec.js
+++ test/ui-core/components/StartNewGame.spec.js
@@ -2,28 +2,40 @@ import { expect } from 'chai';
 import React from 'react';
 import { mount } from 'enzyme';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
-
+import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons';
 import StartNewGame from '../../../src/ui-core/components/StartNewGame';
 
 describe('<StartNewGame />', () => {
-	let startNewGame;
+    let startNewGame;
 
-	beforeEach(() => {
-		startNewGame = mount(<StartNewGame />);
-	});
+    beforeEach(() => {
+        startNewGame = mount(<StartNewGame />);
+    });
 
-	afterEach(() => {
-		startNewGame.unmount();
-	});
+    afterEach(() => {
+        startNewGame.unmount();
+    });
 
-	it('should render the button text', () => {
-		expect(startNewGame.text()).eql('Start New Game');
-	});
+    it('should render the button text', () => {
+        expect(startNewGame.text()).eql('Start New Game');
+    });
 
-	it('should initially render a closed chevron', () => {
-		const fontAwesome = startNewGame.find(FontAwesomeIcon);
+    it('should initially render a closed chevron', () => {
+        assertFontAwesomeIcon(faChevronDown);
+    });
+
+    it('should display an open chevron after being clicked', () => {
+		startNewGame.find('.start-new-game').prop('onClick')();
+
+		startNewGame.update();
 		
-		expect(fontAwesome.prop('icon')).eql(faChevronDown);
-	})
+		assertFontAwesomeIcon(faChevronUp);
+	});
+	
+	function assertFontAwesomeIcon(icon) {
+		const fontAwesome = startNewGame.find(FontAwesomeIcon)
+		
+		expect(fontAwesome.prop('icon')).eql(icon);
+	}
 });
+
