7febdf21abe802c2e1f57dce997840e24d7f5fc3
Cody <codywaukee@gmail.com>
2019-02-11 20:57:59 -0600

    upgrade to babel-7

--- src/ui-core/components/App.js
+++ src/ui-core/components/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import '../sass/components/app.scss';
 class App extends Component {
 	render() {
-		return <span className={'start-new-game'} onClick={() => {}} />;
+		return <span className={'start-new-game'} onClick={() => {}} >Start New Game</span>;
 	}
 }
 export default App;
