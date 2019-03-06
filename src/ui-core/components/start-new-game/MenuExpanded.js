447c1756535a499abf41759e92d71d8641508cfd
Cody <codywaukee@gmail.com>
2019-03-05 18:16:36 -0600

    refactor menu expanded into class in order to introduce mobx store

--- src/ui-core/components/start-new-game/MenuExpanded.js
+++ src/ui-core/components/start-new-game/MenuExpanded.js
@@ -1,27 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronUp } from '@fortawesome/free-solid-svg-icons';
 
 import '../../sass/components/start-new-game/MenuExpanded.scss';
 import TextInput from '../TextInput';
 
-export default function MenuExpanded() {
-
-	return (
-		<div>
-			<TextInput placeholder={'Player Name'} />
-			<FontAwesomeIcon
-				icon={faChevronUp}
-				size={'xs'}
-				className={`chevron`}
-			/>
-			<button
-				onClick={event => {
-					event.stopPropagation();
-				}}
-			>
-				GO
-			</button>
-		</div>
-	);
+export default class MenuExpanded extends Component {
+	render() {
+		return (
+			<div>
+				<TextInput placeholder={'Player Name'} />
+				<FontAwesomeIcon
+					icon={faChevronUp}
+					size={'xs'}
+					className={`chevron`}
+				/>
+				<button
+					onClick={event => {
+						event.stopPropagation();
+					}}
+				>
+					GO
+				</button>
+			</div>
+		);
+	}
 }
