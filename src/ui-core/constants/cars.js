7a60dd3fdf8dab205be0a03018ae91f09e1d9d22
Cody <codywaukee@gmail.com>
2019-03-25 14:34:24 -0500

    add initial car list

---
+++ src/ui-core/constants/cars.js
@@ -0,0 +1,44 @@
+const vehicles = {
+	Honda: {
+		Civic: {
+			year: 1996,
+			trim: 'ex',
+			horsepower: 106,
+			'curb-weight': 2450,
+			price: 900,
+			reliablility: .8
+		}
+	},
+	Mazda: {
+		RX7: {
+			year: 1993,
+			trim: 'base',
+			horsepower: 255,
+			'curb-weight': 2954,
+			price: 2500,
+			reliability: .4
+		}
+	},
+	Ford: {
+		Mustang: {
+			year: 1993,
+			trim: 'GT',
+			horsepower: 225,
+			'curb-weight': 3038,
+			price: 1899,
+			reliability: .6
+		}
+	},
+	Nissan: {
+		'240SX': {
+			year: 1991,
+			trim: 'base',
+			horsepower: 155,
+			'curb-weight': 2800,
+			price: 1300,
+			reliability: .6
+		}
+	},
+};
+
+export default vehicles;
